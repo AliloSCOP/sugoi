9fc62457a5425e6d51ddb3b9a4194fdd808db442
fbarbut <francois.barbut@gmail.com>
2017-08-07 21:44:10 +0200

    fix on stringinput

--- src/sugoi/form/elements/StringInput.hx
+++ src/sugoi/form/elements/StringInput.hx
@@ -6,7 +6,10 @@ class StringInput extends Input<String>
 {
 	
 	override public function getTypedValue(str:String):String{
-		str = StringTools.trim(str);
+		
+		if (str != null) 
+			str = StringTools.trim(str);
+		
 		if (str == "" || str==null) {
 			return null;
 		}else{
